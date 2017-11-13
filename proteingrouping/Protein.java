5551a78c31646a152b6a08449dd6aa1fe7ac809f
Maduka Attamah <paschalpascal@gmail.com>
2017-11-13 23:01:47 +0000

    Update Protein.java

--- proteingrouping/Protein.java
+++ proteingrouping/Protein.java
@@ -1,11 +1,16 @@
+/**
+ *
+ * Copyright 2016-2017 Maduka Attamah
+ *
+ * @author Maduka Attamah
+ *
+ */
+
 package proteomics.proteingrouping;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Maduka Attamah
- */
+
 public class Protein {
 
     private final String dbSequenceRef;
